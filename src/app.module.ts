a4fc48e84494eacb4dde80bbbeef14930ef51804
Mirehiko <mirehiko@gmail.com>
2021-08-02 10:20:06 +0500

    change to modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './controllers/user/user.module';
+import { UserModule } from './modules/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
