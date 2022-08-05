83346dc511c7eb686c92f77542c23f885e915782
mirehiko <mirehiko@gmail.com>
2022-08-05 15:36:46 +0500

    divide on modules and specify ports

--- apps/passport/frontend/jest.config.js
+++ apps/passport/frontend/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'taskapp-frontend',
+  displayName: 'passport-frontend',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/apps/taskapp/frontend',
+  coverageDirectory: '../../../coverage/apps/passport/frontend',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
